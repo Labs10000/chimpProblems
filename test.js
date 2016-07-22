23925b1cc808a8710fe680b63affafbd07356a1b
Keith <kwilson@legacy.com>
2016-07-22 10:19:13 -0700

    Chimp problem testcase

---
+++ test.js
@@ -0,0 +1,16 @@
+
+
+
+describe('New Feature does y', function () {
+
+
+
+it('Go to base URL', function() {
+
+browser.url ( "http://www.google.com/")
+
+});  ;
+it('Clicks on the big red button', function(){}); ;
+it (' Check to verify y happened( fails)', function(){ cats.cat }) ;
+
+});
